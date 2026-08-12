461e6f3d1febb061fd570758db58a2a2ad9ff209
mohammedabdella027 <mohammedabdella027@gmail.com>
2026-08-12 11:02:10 +0300

    feat: added todo list empty state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
+import TodoList from './Components/Main/TodoList'
 
 function App() {
   return (
     <>
     <Header />
+    <TodoList />
     </>
   )
 }
