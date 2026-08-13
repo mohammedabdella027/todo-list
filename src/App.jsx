3ac6d9a158947d032b29b5a4cc213d173128cec5
mohammedabdella027 <mohammedabdella027@gmail.com>
2026-08-13 15:19:22 +0300

    feat: implement dark and light mode with localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
+import { useEffect ,useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
 import TodoList from './Components/Main/TodoList'
+import Search from './Components/Header/Search'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem('theme') === 'dark'
+  })
+
+  useEffect(() => {
+    localStorage.setItem(
+      'theme',
+      darkMode ? 'dark' : 'light'
+    )
+  }, [darkMode])
+
   return (
-    <>
-    <Header />
-    <TodoList />
-    </>
+    <div className={darkMode ? 'bg-[#252525] text-white min-h-screen' : 'bg-white min-h-screen'}>
+    <Header darkMode={darkMode} />
+    
+    <Search 
+    setDarkMode={setDarkMode} 
+    darkMode={darkMode}
+    />
+    
+    <TodoList darkMode={darkMode} />
+    </div>
   )
 }
 export default App
--- src/Components/Header/Search.jsx
+++ src/Components/Header/Search.jsx
@@ -3,7 +3,7 @@ import searchIcon from '../../assets/icons/search.svg'
 import Down from '../../assets/icons/down.png'
 import Moon from '../../assets/icons/moon.svg'
 
-function Search() {
+function Search({darkMode, setDarkMode}) {
     return (
         <div className='flex justify-center gap-4'>
         <div className='relative w-85 sm:w-149 h-9 border border-[#6C63FF] rounded-[5px]'>
@@ -17,7 +17,7 @@ function Search() {
             <p><img src={Down} alt="down" className='w-2 h-1' /></p>
         </button>
 
-        <button className='bg-[#6C63FF] hover:bg-[#5850DD] w-9 h-9 rounded-[5px] cursor-pointer'>
+        <button onClick={() => setDarkMode(!darkMode)} className='bg-[#6C63FF] hover:bg-[#5850DD] w-9 h-9 rounded-[5px] cursor-pointer'>
             <img src={Moon} alt="moon" className='m-auto' />
         </button>
 
