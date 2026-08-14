7291abbc634710d8fa2c1ba175ccc26483d95f8d
mohammedabdella027 <mohammedabdella027@gmail.com>
2026-08-14 10:35:49 +0300

    fix: fixed the small size width of Search component

--- src/Components/Header/Search.jsx
+++ src/Components/Header/Search.jsx
@@ -7,7 +7,7 @@ import Moon from '../../assets/icons/moon.svg'
 function Search({darkMode, setDarkMode}) {
     return (
         <div className='flex justify-center gap-4'>
-        <div className='relative w-85 sm:w-149 h-9 border border-[#6C63FF] rounded-[5px]'>
+        <div className='relative w-55 md:w-149 h-9 border border-[#6C63FF] rounded-[5px]'>
             <input type="text" placeholder='Search note...' className='placeholder:text-[#C3C1E5] w-full h-full outline-none font-medium text-[16px] pl-4 text-[#6C63FF]'/>
 
             <button className='absolute right-4 top-1.5 cursor-pointer'><img src={searchIcon} alt="Search" /></button>
